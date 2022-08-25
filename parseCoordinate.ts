5afd2d8b536b6f5d4639c2dc480d6f9684f7a8c2
Idris Yakub <olayinkayakub@yahoo.com>
2022-08-25 23:07:34 +0100

    function overloading

---
+++ parseCoordinate.ts
@@ -0,0 +1,23 @@
+type Coordinate = {
+    x: number;
+    y: number;
+}
+
+function parseCoordinateFromObject(obj: Coordinate): Coordinate {
+    return{
+        ...obj,
+    }
+}
+
+function parseCoordinateFromNumbers(x: number, y: number): Coordinate{
+    return{
+        x,
+        y,
+    }
+}
+
+function parseCoordinate(obj: Coordinate): Coordinate;
+function parseCoordinate(x: number, y: number): Coordinate;
+function parseCoordinate(arg1: unknown, arg2: unknown): Coordinate{
+    
+}
